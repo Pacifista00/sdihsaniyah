edb57271f91d9612b82f70c881438d41fb89e025
pacifista00 <adamsamudera99@gmail.com>
2025-08-30 07:27:28 +0700

    revisi

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -6,10 +6,9 @@
             <script>
                 document.write(new Date().getFullYear());
             </script>
-            , made with ❤️ by
-            <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+            , SD Ihsaniyah 1 Kota Tegal
         </div>
-        <div>
+        {{-- <div>
             <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
             <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More
                 Themes</a>
@@ -19,7 +18,7 @@
 
             <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
                 class="footer-link me-4">Support</a>
-        </div>
+        </div> --}}
     </div>
 </footer>
 <!-- / Footer -->
